de2f53b4c3a9fa09be8395f90b6483190a580ed0
aryan <aryanpidiha.dev@gmail.com>
2024-09-26 08:40:51 +0530

    added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,30 @@ const Footer = () => {
   return (
     <div className="flex flex-col xs:pt-10 lg:pt-[80px] pb-[38px] px-6 xl:px-[90px]">
       <div></div>
-      <div></div>
+      <div className="flex flex-col gap-8">
+        <div className="flex w-full">
+          <hr
+            className="border-[1px] border-[#11101033] w-full
+          "
+          />
+        </div>
+        <div className="flex justify-between gap-4 items-center w-full flex-wrap">
+          <div className="font-base font-normal text-[#111010]">
+            {" "}
+            Copyright ©2023 Red Bangle
+          </div>
+          <div className="flex gap-4 lg:gap-8 ">
+            #111010
+            <div className="font-base font-normal text-[#111010]">
+              Privacy Policy
+            </div>
+            <div className="font-base font-normal text-[#111010]">Legal</div>
+            <div className="font-base font-normal text-[#111010]">
+              Terms & Conditions
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
